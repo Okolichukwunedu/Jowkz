b4aa384159ff9cd2c6046040beb01f6fe4b4f06d
Okoli Chinedu Christian <Okolichukwunedu@gmail.com>
2022-11-18 14:52:13 -0800

    Add files via upload

---
+++ Jowkz/Jowkz/Controller/PhotoCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCell.swift
+//  Jowkz
+//
+//  Created by Okoli-Chinedu on 16/11/2022.
+//  Copyright © 2022 Okoli-Chinedu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    //MARK: Outlet
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
